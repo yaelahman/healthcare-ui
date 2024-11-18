6137814e9d81aab9eb121b8a0b4a1469f1319f68
Manzcode <yaelahman0810@gmail.com>
2024-11-18 20:25:21 +0700

    init

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#002952',
+                badgeGrey: '#F4F7F9',
+                cardHeader: '#F4F7F9',
+                darkBlue: '#002952',
+                black: '#111111',
+                muted: '#8A94A4'
+            },
+        },
+    },
+};
+
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const mainContent = document.getElementById('main-content');
+    const sidebarTextElements = document.querySelectorAll('.sidebar-text'); // All text elements in sidebar
+
+    sidebar.classList.toggle('sidebar-hidden');
+    sidebar.classList.toggle('sidebar-visible');
+
+    mainContent.classList.toggle('main-content-expanded');
+    mainContent.classList.toggle('main-content-collapsed');
+
+    // Hide or show text depending on the sidebar state
+    sidebarTextElements.forEach(text => {
+        text.classList.toggle('hide-text');
+    });
+}
+
+function changeTab(tabName) {
+    // Remove the 'active' class from all tabs
+    document.querySelectorAll('.tab').forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    // Add 'active' class to the clicked tab
+    document.getElementById(`tab-${tabName}`).classList.add('active');
+
+    // Hide all tab content sections
+    document.querySelectorAll('.tab-content').forEach(tabContent => {
+        tabContent.classList.add('hidden');
+    });
+
+    // Show the content of the clicked tab
+    document.getElementById(tabName).classList.remove('hidden');
+
+    window.history.pushState({}, "", `${window.location.pathname}?page=${tabName}`);
+}
+
+const page = window.location.search.split('page=')[1]
+if (page) {
+    setTimeout(() => {
+        changeTab(page)
+    }, 1)
+}
+
+function toggleSubTabs(subTabId) {
+    document.getElementById(subTabId).classList.toggle('hidden');
+} 
